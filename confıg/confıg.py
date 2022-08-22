e71f6d2f6e4435260fe5f978f99dddedba2354a9
Løse Ønline <108939077+usermusti@users.noreply.github.com>
2022-08-23 01:43:27 +0300

    Create confıg.py

---
+++ confıg/confıg.py
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2021-2022 by TeamLose@Github, < https://github.com/usermusti >.
+#
+# This file is part of < https://github.com/usermusti/LoseMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/usermusti/LoseMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+
+from .config import *
