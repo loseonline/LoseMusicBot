237c430440e2ad1c3fb253432212aeefd842d775
Løse Ønline <108939077+usermusti@users.noreply.github.com>
2022-08-23 01:47:33 +0300

    Update helpers.py

--- strings/helpers.py
+++ strings/helpers.py
@@ -1,7 +1,7 @@
 #
 # Copyright (C) 2021-2022 by TeamLose@Github, < https://github.com/usermusti >.
 #
-# This file is part of < https://github.com/usermusti/LoseMusicBot> project,
+# This file is part of < https://github.com/usermusti/LoseMusicBot > project,
 # and is released under the "GNU v3.0 License Agreement".
 # Please see < https://github.com/usermusti/LoseMusicBot/blob/master/LICENSE >
 #
