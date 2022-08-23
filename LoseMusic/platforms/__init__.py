421487e67504e746e46a9ff8c6af89f386ff6cfb
Løse Ønline <108939077+usermusti@users.noreply.github.com>
2022-08-23 03:57:21 +0300

    Create __init__.py

---
+++ LoseMusic/platforms/__init__.py
@@ -0,0 +1,16 @@
+#
+# Copyright (C) 2021-2022 by TeamLose@Github, < https://github.com/usermusti >.
+#
+# This file is part of < https://github.com/usermusti/LoseMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/usermusti/LoseMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
+
+from .Apple import AppleAPI
+from .Carbon import CarbonAPI
+from .Resso import RessoAPI
+from .Soundcloud import SoundAPI
+from .Spotify import SpotifyAPI
+from .Telegram import TeleAPI
+from .Youtube import YouTubeAPI
